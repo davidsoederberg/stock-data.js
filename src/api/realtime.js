4ba4facdd8a520d5178bfe1364350894370652f2
David Söderberg <davidsoderberg@comhem.se>
2019-02-02 01:29:10 +0100

    fix: refactor code

--- src/api/realtime.js
+++ src/api/realtime.js
@@ -9,17 +9,24 @@ async function multipleSymbols(URL, symbols, API_TOKEN) {
   for (let i = 0; i < iterations; i += 1) {
     const currentIndex = 5 * i;
     const query = symbols.slice(currentIndex, currentIndex + 5).join(',');
-    try {
-      const fetchedData = fetchData(URL, query, API_TOKEN);
-      data.push(fetchedData);
-    } catch (error) {
-      throw error;
-    }
+    const fetchedData = fetchData(URL, query, API_TOKEN);
+    data.push(fetchedData);
   }
   return Promise.all(data);
 }
 
+function mergeData(fetchedData) {
+  const mergedData = { symbols_requested: 0, symbols_returned: 0, data: [] };
+  fetchedData.forEach((dataObject) => {
+    mergedData.symbols_requested += dataObject.symbols_requested;
+    mergedData.symbols_returned += dataObject.symbols_returned;
+    mergedData.data = mergedData.data.concat(dataObject.data);
+  });
+  return mergedData;
+}
+
 async function realtime({ symbols, API_TOKEN, FUND }) {
+  const API_TOKEN_URL = `&api_token=${API_TOKEN}`;
   const URL = FUND ? FUND_URL : STOCK_URL;
   if (symbols === undefined) {
     throw new Error('No symbols provided, add symbols as an argument');
@@ -28,18 +35,10 @@ async function realtime({ symbols, API_TOKEN, FUND }) {
     throw new Error('No API_TOKEN provided, add your API_TOKEN from https://www.worldtradingdata.com/ as an argument');
   }
   try {
-    const API_TOKEN_URL = `&api_token=${API_TOKEN}`;
     if (typeof symbols === 'string') {
       return await fetchData(URL, symbols, API_TOKEN_URL);
     }
-    const mergeData = { symbols_requested: 0, symbols_returned: 0, data: [] };
-    const fetchedData = await multipleSymbols(URL, symbols, API_TOKEN_URL);
-    for (const dataObject of fetchedData) { // eslint-disable-line no-restricted-syntax
-      mergeData.symbols_requested += dataObject.symbols_requested;
-      mergeData.symbols_returned += dataObject.symbols_returned;
-      mergeData.data = mergeData.data.concat(dataObject.data);
-    }
-    return mergeData;
+    return mergeData(await multipleSymbols(URL, symbols, API_TOKEN_URL));
   } catch (error) {
     throw new Error(error);
   }
